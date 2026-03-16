7167fc85837824f49514fcf9be39ac0b5049e320
Benja-Dev <begalan@outlook.es>
2026-03-16 00:21:55 -0600

    related items

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Package;
+use App\Models\Promotion;
+use App\Models\Service;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::morphMap([
+            'service' => Service::class,
+            'promotion' => Promotion::class,
+            'package' => Package::class,
+        ]);
     }
 }
