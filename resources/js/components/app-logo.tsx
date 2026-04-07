7b84bbb7de4ba800462efccdacaad9919af729e2
Benja-Dev <begalan@outlook.es>
2026-04-07 09:14:59 -0600

    actualizando logo de la app

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,11 +1,7 @@
-import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon />
-            </div>
+            <img src="/logo.png" alt="Uñas & Mechas Logo" className="size-9 object-contain rounded-md" />
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
                     Uñas & Mechas
